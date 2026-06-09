399266ef9ebc56f235e84a15fd40e794aee49778
jimmymuthoni <jimmymuthoni26@gmail.com>
2026-06-09 16:55:11 +0300

    file for creating resources

--- day4/main.tf
+++ day4/main.tf
@@ -11,3 +11,14 @@
  resource "aws_s3_bucket" "mybacket" {
    bucket = "jim-s3-demo-dev-ops"
  }
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name           = "terraform-lock"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
